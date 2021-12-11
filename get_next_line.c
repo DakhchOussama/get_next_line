fc5856d5472dc9a17500c7078d292a89db72b208
DakhchOussama <48569176+DakhchOussama@users.noreply.github.com>
2021-12-11 13:41:55 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 
 int	ft_line(char	*tab)
 {
